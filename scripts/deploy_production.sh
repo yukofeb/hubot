35c618dfda88d8cdada53c9ecf2d683836013464
yukofeb <yuko.february@gmail.com>
2015-08-20 17:05:38 -0400

    modify

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -4,7 +4,7 @@ export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 export AWS_DEFAULT_REGION="ap-northeast-1"
 
-MYSECURITYGROUP=”sg-97d4a6f2"
+MYSECURITYGROUP="sg-97d4a6f2"
 MYIP=`curl -s ifconfig.me`
 
 aws ec2 authorize-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
