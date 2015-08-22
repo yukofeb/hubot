3b7d0cbe712143163f9aebe1a8f3305b87924a8e
yukofeb <yuko.february@gmail.com>
2015-08-22 09:25:53 -0400

    modify

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -8,5 +8,5 @@ MYSECURITYGROUP="sg-97d4a6f2"
 MYIP=`curl -s ifconfig.me`
 
 aws ec2 authorize-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
-bundle exec cap production deploy
+cap production deploy
 aws ec2 revoke-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
